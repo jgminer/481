0bc99c25abb50a7da7459a3d8851fd01f5206826
jack <jack@jack-ThinkPad-X201.(none)>
2014-01-25 19:07:51 -0500

    initial HW3 commit

---
+++ eclipse/touchtest/src/com/example/touchtest/Touch.java
@@ -0,0 +1,98 @@
+package com.example.touchtest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+
+public class Touch extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_touch);
+		// Show the Up button in the action bar.
+		setupActionBar();
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.touch, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public class TouchExampleView extends View{
+		private Drawable mIcon;
+		private float mPosX;
+		private float mPosY;
+		
+		//private float mLastTouchX;
+		//private float mLastTouchY;
+		
+		public TouchExampleView(Context context){
+			this(context, null, 0);
+		}
+		public TouchExampleView(Context context, AttributeSet attrs){
+			this(context, attrs, 0);
+		}
+		public TouchExampleView(Context context, AttributeSet attrs, int defStyle){
+			super(context, attrs, defStyle);
+			mIcon = context.getResources().getDrawable(com.example.touchtest.R.drawable.icon);
+			mIcon.setBounds(0, 0, mIcon.getIntrinsicWidth(), mIcon.getIntrinsicHeight());
+		}
+		
+		@Override
+		public void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+			
+			canvas.save();
+			canvas.translate(mPosX, mPosY);
+			mIcon.draw(canvas);
+			canvas.restore();
+		}
+		
+		@Override
+		public boolean onTouchEvent(MotionEvent ev){
+			return true;
+		}
+	}
+
+
+}
